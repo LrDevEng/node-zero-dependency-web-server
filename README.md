9fbe113faf0f3869c405f7315ba94ec8b94fbde6
LrDevEng <lr.deveng@gmail.com>
2024-09-12 21:13:23 +0200

    Add mock-up html page in public directory Add comments in index.js

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # node-zero-dependency-web-server
+
 Simple web server only using node.js standard library.
+Server handles http get request from client and provides files located in directory 'public'.
+
+# Served Files
+
+- .html
+- .htm
+- .css
+- .js
+- .txt
+- .jpg
+- .jpeg
+- .gif
+- .png
+- .svg
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const PORT = 3000;
 const DIR = './public';
 const BASE_FILE = 'index.html';
 
+// Object to look up http content-type based on requested file extension
 const extToContentType = {
   '.html': 'text/html',
   '.htm': 'text/html',
@@ -21,6 +22,7 @@ const extToContentType = {
 
 const server = http.createServer((req, res) => {
   if (req.method === 'GET') {
+    // Parse endpoint based on requested url
     const url = req.url;
     const extname = path.extname(url);
     const endpoint =
@@ -29,19 +31,23 @@ const server = http.createServer((req, res) => {
         : DIR + (url.endsWith('/') ? url : url + '/') + BASE_FILE;
 
     if (fs.existsSync(endpoint)) {
+      // Setup positiv response head with content type based on file extension
       res.writeHead(200, {
         'Content-type': extToContentType[path.extname(endpoint)],
       });
+      // Read requested endpoint file from file system and pass to response
       fs.createReadStream(endpoint)
         .on('error', (error) => {
           console.log(error.message);
         })
         .pipe(res);
     } else {
+      // Respond with error in case requested endpoint does not exist
       res.writeHead(404, { 'Content-type': 'text/plain' });
       res.end('404: No such file or directory.');
     }
   }
 });
 
+// Start server and listen on specified port
 server.listen(PORT);
