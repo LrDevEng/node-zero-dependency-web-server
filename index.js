c88f9d8d5402caa0c85254042bc7a167ecaee914
LrDevEng <lr.deveng@gmail.com>
2024-09-12 15:51:20 +0200

    Update index.js Add functionality to allocate content type based on file extension

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+import fs from 'node:fs';
 import http from 'node:http';
 import path from 'node:path';
 
 const PORT = 3000;
 const DIR = './public';
 const BASE_FILE = 'index.html';
+
+const extToContentType = {
+  '.html': 'text/html',
+  '.htm': 'text/html',
+  '.css': 'text/css',
+  '.js': 'text/javascript',
+  '.txt': 'text/plain',
+  '.jpg': 'image/jpeg',
+  '.jpeg': 'image/jpeg',
+  '.gif': 'image/gif',
+  '.png': 'image/png',
+  '.svg': 'image/svg+xml',
+};
 
 const server = http.createServer((req, res) => {
   if (req.method === 'GET') {
@@ -14,13 +28,20 @@ const server = http.createServer((req, res) => {
         ? DIR + url
         : DIR + (url.endsWith('/') ? url : url + '/') + BASE_FILE;
 
-    console.log(endpoint);
+    if (fs.existsSync(endpoint)) {
+      res.writeHead(200, {
+        'Content-type': extToContentType[path.extname(endpoint)],
+      });
+      fs.createReadStream(endpoint)
+        .on('error', (error) => {
+          console.log(error.message);
+        })
+        .pipe(res);
+    } else {
+      res.writeHead(404, { 'Content-type': 'text/plain' });
+      res.end('404: No such file or directory.');
+    }
   }
-
-  res.statusCode = 200;
-  res.setHeader('content-type', 'text/html');
-  res.write('Hello client.');
-  res.end();
 });
 
 server.listen(PORT);
