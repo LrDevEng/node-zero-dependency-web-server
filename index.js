e4bc86b63d16afbab06b88bc07fda2975f0272a6
LrDevEng <lr.deveng@gmail.com>
2024-09-12 15:05:12 +0200

    Update index.js Construct endpoint based on http request

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 import http from 'node:http';
+import path from 'node:path';
 
 const PORT = 3000;
+const DIR = './public';
+const BASE_FILE = 'index.html';
 
 const server = http.createServer((req, res) => {
+  if (req.method === 'GET') {
+    const url = req.url;
+    const extname = path.extname(url);
+    const endpoint =
+      extname.length > 0
+        ? DIR + url
+        : DIR + (url.endsWith('/') ? url : url + '/') + BASE_FILE;
+
+    console.log(endpoint);
+  }
+
   res.statusCode = 200;
   res.setHeader('content-type', 'text/html');
   res.write('Hello client.');
